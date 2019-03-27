669708c2463fd8f75c5ecab6e08d9a0cd2fe238a
MoranOshia <44996337+MoranOshia@users.noreply.github.com>
2019-03-27 20:38:04 +0200

    Update Tree.cpp

--- Tree.cpp
+++ Tree.cpp
@@ -1,250 +1,279 @@
-#include <iostream>
-#include <math.h>
-#include <algorithm>
-#include "Tree.hpp"
+
+	/**
+	 * Examples of automatic tests for the exercise on binary trees.
+	 *
+	 * @author Erel Segal-Halevi
+	 * @since 2019-02
+	 */
+
+	#include <iostream>
+	#include <math.h>
+	#include <algorithm>
+	#include "Tree.hpp"
 
 
 
-using namespace std;
-using namespace ariel;
+	using namespace std;
+	using namespace ariel;
 
 
 
-  Node::Node(){
-      this->value=0;
-  }
-   Node::Node(int val) {
-        this->value = val;
+	Node::Node(){
+		//default Constructor
+		this->value=0;
+	}
+	Node::Node(int val) {
+		// Constructor by value
+		this->value = val;
 		this->left = NULL;
-        this->right = NULL;
-    }
+		this->right = NULL;
+	}
 
-    Node::Node(int val, Node *left, Node *right) {
-        this->value = val;
-        this->left = left;
-        this->right = right;
+	Node::Node(int val, Node *left, Node *right) {
+		//Copy Constructor
+		this->value = val;
+		this->left = left;
+		this->right = right;
+	}
+
+	Tree::Tree(){
+		//default Constructor
+        roo = NULL;
     }
+	Tree::~Tree()
+	{//D constructor
+		destroyTree(this->roo);
+	}
+
+	void Tree::destroyTree(Node* leaf)
+	{//Deleting all the Tree for Memory leak
+		if(leaf!=NULL)
+		{
+			destroyTree(leaf->left);
+			destroyTree(leaf->right);
+			delete leaf;
+		}
+	}
+	void Tree::addHelper(Node *root, int val) {
+		//Function that add new node by value we get 
+		if (root->value > val) {
+			if (!root->left) {
+				root->left = new Node(val);
+			} else {
+				addHelper(root->left, val);
+			}
+		} else {
+			if (!root->right) {
+				root->right = new Node(val);
+			} else {
+				addHelper(root->right, val);
+			}
+		}
+	}
 
 
-    void Tree::addHelper(Node *root, int val) {
-	
-        if (root->value > val) {
-            if (!root->left) {
-                root->left = new Node(val);
-            } else {
-                addHelper(root->left, val);
-            }
-        } else {
-            if (!root->right) {
-                root->right = new Node(val);
-            } else {
-                addHelper(root->right, val);
-            }
-        }
-    }
-   
-    
-    void Tree::printHelper(Node *root) {
-        //postOrder
-        if (!root) return;
-        printHelper(root->left);
-        cout<<root->value<<' ';
-        printHelper(root->right);
-    }
-ariel::Tree e;
-    int Tree::nodesCountHelper(Node *root) {
-        if (!root) return 0;
-        else return 1 + nodesCountHelper(root->left) + nodesCountHelper(root->right);
-    }
+	void Tree::printHelper(Node *root) {
+		//postOrder printing Tree
+		if (!root) return;
+		printHelper(root->left);
+		cout<<root->value<<' ';
+		printHelper(root->right);
+	}
 
-    int Tree::heightHelper(Node *root) {
-        if (!root) return 0;
-        else return 1 + std::max(heightHelper(root->left), heightHelper(root->right));
-    }
+	int Tree::nodesCountHelper(Node *root) {
+		//Function counting size of the tree
+		if (!root) return 0;
+		else return 1 + nodesCountHelper(root->left) + nodesCountHelper(root->right);
+	}
 
-    void Tree::printMaxPathHelper(Node *root) {
-        if (!root) return;
-        cout<<root->value<<' ';
-        if (heightHelper(root->left) > heightHelper(root->right)) {
-            printMaxPathHelper(root->left);
-        } else {
-            printMaxPathHelper(root->right);
-        }
-    }
+	int Tree::heightHelper(Node *root) {
+		if (!root) return 0;
+		else return 1 + std::max(heightHelper(root->left), heightHelper(root->right));
+	}
 
-    bool Tree::deleteValueHelper(Node *parent, Node *current, int value) {
-        if (!current) return false;
-        if (current->value == value) {
-            if (current->left == NULL || current->right == NULL) {
-                Node *temp = current->left;
-                if (current->right) temp = current->right;
-                if (parent) {
-                    if (parent->left == current) {
-                        parent->left = temp;
-                    } else {
-                        parent->right = temp;
-                    }
-                } else {
-                    this->roo = temp;
-                }
-            } else {
-                Node *validSubs = current->right;
-                while (validSubs->left) {
-                    validSubs = validSubs->left;
-                }
-                int temp = current->value;
-                current->value = validSubs->value;
-                validSubs->value = temp;
-                return deleteValueHelper(current, current->right, temp);
-            }
-            delete current;
-            return true;
-        }
-        return deleteValueHelper(current, current->left, value) || deleteValueHelper(current, current->right, value);
-    }
+	void Tree::printMaxPathHelper(Node *root) {
+		//Function Counting maximum path of Tree
+		if (!root) return;
+		cout<<root->value<<' ';
+		if (heightHelper(root->left) > heightHelper(root->right)) {
+			printMaxPathHelper(root->left);
+		} else {
+			printMaxPathHelper(root->right);
+		}
+	}
 
-    bool containsNode(Node* top, int data) {
+	bool Tree::deleteValueHelper(Node *parent, Node *current, int value) {
+		//Function deleting value we get from tree
+		if (!current) return false; //check isn't NULL
+		if (current->value == value) { //Check if we in the current value
+			if (current->left == NULL || current->right == NULL) {
+				Node *temp = current->left;
+				if (current->right) temp = current->right;
+				if (parent) {
+					if (parent->left == current) {
+						parent->left = temp;
+					} else {
+						parent->right = temp;
+					}
+				} else {
+					this->roo = temp;
+				}
+			} else { 
+				Node *validSubs = current->right;
+				while (validSubs->left) {//goes to the most lef Node
+					validSubs = validSubs->left;
+				}
+				int temp = current->value;
+				current->value = validSubs->value;
+				validSubs->value = temp;
+				return deleteValueHelper(current, current->right, temp);//recorsive
+			}
+			delete current;
+			return true;
+		}
+		return deleteValueHelper(current, current->left, value) || deleteValueHelper(current, current->right, value);//recorsive
+	}
+
+	bool Tree::containsNode(Node* top, int data) {
+		//Boolean Function check if data is in the Tree
 		if (top == NULL) return false;
 		if (data == top->value) return true;
 		if (data < top->value)  return containsNode(top->left, data);    
 		return containsNode(top->right, data);
 	}
 
-	Node* getParent(Node *root, int key){
-		
-	if (root == NULL){
-		return NULL;
-	}
-	else if ( (root->left!=NULL && root->left->value == key) || ((root->right!=NULL) && (root->right->value == key)))
-	{
+	Node* Tree::getParent(Node *root, int key){
+		//Function returning the parent of key we got
+		if (root == NULL){//check if NULL
+			return NULL;
+		}
+		else if ( (root->left!=NULL && root->left->value == key) || (root->right!=NULL && root->right->value == key) //checking root
+		{
 			return root;
 		}
 		else {
-			Node* parentN= getParent(root->left, key);
+			Node* parentN= getParent(root->left, key);//recorsive left size 
 			if (parentN!=NULL){
 				return parentN;
 			}
 			else{
-				return getParent(root->right, key);
+				return getParent(root->right, key);//recorsive right size 
 			}
 		}
 
 	}
-	
-	Node* getNodeByIndex(Node* top, int data) {
-		
+
+	Node* Tree::getNodeByIndex(Node* top, int data) {
+		//Getting node by data we get
 		if (top == NULL) return NULL;
 		if (data == top->value) return top;
 		if (data < top->value)  return getNodeByIndex(top->left, data);    
 		return getNodeByIndex(top->right, data);
 	}
-	
-    Tree::Tree(){
-        roo = NULL;
-    }
-    void Tree::insert(int i){
-        if(contains(i) == true)
+
+
+	void Tree::insert(int i){
+		if(contains(i) == true)
 		{
-			 throw std::invalid_argument( "Value already exists" );
+			throw std::invalid_argument( "Value already exists" );
 		}
 		else{
-          if (roo) {
-            this->addHelper(roo, i);
-        } else {
-            roo = new Node(i);
-			   }
+			if (roo) {
+				this->addHelper(roo, i);
+			} else {
+				roo = new Node(i);
+			}
 		}
-    }
+	}
 
-    void Tree::remove(int i){
+	void Tree::remove(int i){
 		if(contains(i) == false)
 		{
-         throw std::invalid_argument( "value not exist" );
+			throw std::invalid_argument( "value not exist" );
 		}
 		else{
-        bool flag= this->deleteValueHelper(NULL, this->roo, i);
+			bool flag= this->deleteValueHelper(NULL, this->roo, i);
 		}
-    }
+	}
 
-    int Tree::size(){
-       return nodesCountHelper(roo);
-    }
+	int Tree::size(){
+		return nodesCountHelper(roo);
+	}
 
-    bool Tree::contains(int i){
-        bool con = containsNode(roo, i);
-        return con;
-		
-		
-    }
+	bool Tree::contains(int i){
+		bool con = containsNode(roo, i);
+		return con;
 
-    int Tree::root(){
+
+	}
+
+	int Tree::root(){
 		if(this->roo!=NULL){
-         return this->roo->value;
+			return this->roo->value;
 		}
 		else
 		{
 			throw std::invalid_argument( "WORNG root is NULL" );
 		}
-    }
+	}
 
-    int Tree::parent(int i){
+	int Tree::parent(int i){
 		if(contains(i)==true){
-		Node* parent=getParent(this->roo, i);
-     
-		if(parent!=NULL){
-			return parent->value;
-		}
-		else
-		{
-			throw std::invalid_argument( "WORNG dont have parents" );
-		}
+			Node* parent=getParent(this->roo, i);
+
+			if(parent!=NULL){
+				return parent->value;
+			}
+			else
+			{
+				throw std::invalid_argument( "WORNG dont have parents" );
+			}
 		}
 		else
 		{
 			throw std::invalid_argument( "Not have key in the tree" );
-			
-		}
-    }
 
-    int Tree::left(int i){
-		if(contains(i)==true){
-			
-        Node* leftN=getNodeByIndex(this->roo, i);
-		if(leftN!=NULL && leftN->left!=NULL){
-			return leftN->left->value;
 		}
-		else
-		{
-			throw std::invalid_argument( "The right Node is NULL" );
-		}
-		}
-		else
-		{
-			throw std::invalid_argument( "Not have key in the tree" );
-		}
-    }
+	}
 
-    int Tree::right(int i){
+	int Tree::left(int i){
 		if(contains(i)==true){
 
-        Node* n=getNodeByIndex(this->roo, i);
-		
-		if(n!=NULL && n->right!=NULL ){
-			return n->right->value;
-		}
-		else
-		{
-			throw std::invalid_argument( "The right Node is NULL" );
-		}
+			Node* leftN=getNodeByIndex(this->roo, i);
+			if(leftN!=NULL && leftN->left!=NULL){
+				return leftN->left->value;
+			}
+			else
+			{
+				throw std::invalid_argument( "The right Node is NULL" );
+			}
 		}
 		else
 		{
 			throw std::invalid_argument( "Not have key in the tree" );
 		}
-    }
+	}
 
-    void Tree::print(){
-       printHelper(this->roo);
-    }
- 
- 
+	int Tree::right(int i){
+		if(contains(i)==true){
+
+			Node* n=getNodeByIndex(this->roo, i);
+
+			if(n!=NULL && n->right!=NULL ){
+				return n->right->value;
+			}
+			else
+			{
+				throw std::invalid_argument( "The right Node is NULL" );
+			}
+		}
+		else
+		{
+			throw std::invalid_argument( "Not have key in the tree" );
+		}
+	}
+
+	void Tree::print(){
+		printHelper(this->roo);
+	}
+
