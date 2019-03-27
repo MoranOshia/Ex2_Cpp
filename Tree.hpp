e33839c356056c81d6384f75a5fdd58312491f38
MoranOshia <44996337+MoranOshia@users.noreply.github.com>
2019-03-27 20:09:19 +0200

    Update Tree.hpp

--- Tree.hpp
+++ Tree.hpp
@@ -18,16 +18,16 @@ class Tree{
     public:
     
     Tree();
-	~Tree();
+    ~Tree();
     void addHelper(Node *root, int val);
     void printHelper(Node *root);
     int nodesCountHelper(Node *root);
     int heightHelper(Node *root);
     void printMaxPathHelper(Node*root);
     bool deleteValueHelper(Node *parent, Node *current, int value);
-	bool containsNode(Node* top, int data) ;
-	Node* getParent(Node *root, int key);
-	Node* getNodeByIndex(Node* top, int data);
+    bool containsNode(Node* top, int data) ;
+    Node* getParent(Node *root, int key);
+    Node* getNodeByIndex(Node* top, int data);
     void insert(int i);
     void remove(int i);
     int size();
@@ -37,6 +37,6 @@ class Tree{
     int left(int i);
     int right(int i);
     void print();
-	void destroyTree(Node* leaf);
+    void destroyTree(Node* leaf);
     };
 }
